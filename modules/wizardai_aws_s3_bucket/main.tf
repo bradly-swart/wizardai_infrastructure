9b287fc4967545260ace129ef63703d6adb733af
Your Name <brad8711@gmail.com>
2024-10-28 23:25:07 +1000

    add encryption in transit

--- modules/wizardai_aws_s3_bucket/main.tf
+++ modules/wizardai_aws_s3_bucket/main.tf
@@ -38,3 +38,22 @@ resource "aws_s3_bucket_public_access_block" "private" {
   ignore_public_acls      = true
   restrict_public_buckets = true # This could affect cross account access/replication, may need to come back it if that requirement comes up.
 }
+
+resource "aws_s3_bucket_policy" "this" {
+  count = var.create_bucket ? 1 : 0
+
+  bucket = aws_s3_bucket.this[0].id
+  policy = data.aws_iam_policy_document.encryption_in_transit[0].json
+}
+
+# TODO:
+# versioning
+# object locking
+# update bucket policy to allow user to configure it (bring princiapl IAM role)
+# lifecycle rules to manage cost
+# cloudfront OAI ?
+# cross-region replication
+
+# Further consideration:
+# monitoring - 4xx, deletes, acl changes
+# auditing - access logging
--- modules/wizardai_aws_s3_bucket/s3_encryption.tf
+++ modules/wizardai_aws_s3_bucket/s3_encryption.tf
@@ -11,3 +11,65 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "kms" {
     }
   }
 }
+
+# Policy sourced from:
+# https://repost.aws/knowledge-center/s3-bucket-policy-for-config-rule
+# https://repost.aws/knowledge-center/s3-enforce-modern-tls
+
+data "aws_iam_policy_document" "encryption_in_transit" {
+  count = var.create_bucket ? 1 : 0
+
+  statement {
+    sid    = "EnforceTLSv12orHigher"
+    effect = "Deny"
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      aws_s3_bucket.this[0].arn,
+      "${aws_s3_bucket.this[0].arn}/*",
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "NumericLessThan"
+      variable = "s3:TlsVersion"
+      values = [
+        "1.2"
+      ]
+    }
+  }
+
+  statement {
+    sid    = "AllowSSLRequestsOnly"
+    effect = "Deny"
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      aws_s3_bucket.this[0].arn,
+      "${aws_s3_bucket.this[0].arn}/*",
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values = [
+        "false"
+      ]
+    }
+  }
+}
